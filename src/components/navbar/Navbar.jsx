d23ea8c00ac96083afcabc3d9c7e3e728b90541e
gastonMacias <gastonezequielmacias@gmail.com>
2025-03-17 18:20:58 -0300

    primeros pasos de componente Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="bg-pink-300 h-40 flex items-center justify-around ">
+                <div className="flex flex-col items-center ">
+                    <img className="size-20 w-fit rounded-4xl  " src="../public/logo/logo sin fondo.jpg" alt="logo valky" />
+                    <h1 className="text-black font-thin text-3xl ">Libreria Creativa Valky</h1>
+                </div>
+                <form className="">
+                    <textarea className=" bg-amber-50 w-70 h-8 rounded-lg" />
+                </form>
+            </nav>
+
+        </>
+    )
+}
+
+
+export default Navbar;
