161a3fe74642a83b7980fb1db4b37b2d7cf60548
gastonMacias <gastonezequielmacias@gmail.com>
2025-03-18 18:02:33 -0300

    componente productos primeros estilos

--- src/components/productos/Productos.jsx
+++ src/components/productos/Productos.jsx
@@ -1,7 +1,27 @@
+import Cards from "../cards/Cards";
 
 const Productos = () => {
     return (
         <>
+            <main className="flex flex-col gap-8 bg-gray-200 p-6 ">
+                <h3 className="text-center text-pink-400 font-bold text-3xl">Nuestros Productos</h3>
+                <div className="flex-col ">
+                    <h3 className="text-left text-pink-400 font-bold text-2xl bg-gray-100 m-3 p-3 rounded">Cuadernos A4</h3>
+                    <div className="flex items-center justify-center gap-4 p-5">
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                    </div>
+                    <h3 className="text-left text-pink-400 font-bold text-2xl bg-gray-100 m-3 p-3 rounded">Cuadernos A5</h3>
+                    <div className="flex items-center justify-center gap-4 p-5">
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                        <Cards />
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
