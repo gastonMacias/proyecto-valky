c7fa79787c311c0c7edc9425864d1039121bbc85
gastonMacias <gastonezequielmacias@gmail.com>
2025-03-18 18:00:59 -0300

    se agregaron los componentes productos y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Productos from './components/productos/Productos'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Productos />
+      <Footer />
     </>
   )
 }
