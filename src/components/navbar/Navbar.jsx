3f6a6da38f81f4870b7cf6183d9a611d76509504
gastonMacias <gastonezequielmacias@gmail.com>
2025-03-18 18:02:06 -0300

    cambio de h1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <nav className="bg-pink-300 h-40 flex items-center justify-around ">
                 <div className="flex flex-col items-center ">
                     <img className="size-20 w-fit rounded-4xl  " src="../public/logo/logo sin fondo.jpg" alt="logo valky" />
-                    <h1 className="text-black font-thin text-3xl ">Libreria Creativa Valky</h1>
+                    <h1 className="text-black font-thin text-3xl">Papelería Creativa Valky</h1>
                 </div>
                 <div>
                     <SearchInput />
@@ -23,7 +23,3 @@ const Navbar = () => {
 export default Navbar;
 
 
-
-{/* <form className="">
-                    <textarea className=" bg-amber-50 w-70 h-8 rounded-lg" />
-                </form> */}
