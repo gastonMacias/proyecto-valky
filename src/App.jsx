6dfea9f52ac4068edfe145f57c34460a181e887e
gastonMacias <gastonezequielmacias@gmail.com>
2025-03-31 17:28:44 -0300

    se agrego carrousel y emprolijo imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+
 import './App.css'
 import Navbar from './components/navbar/Navbar'
+import Carrousel from './components/carrousel/Carrousel'
 import Productos from './components/productos/Productos'
 import Footer from './components/footer/Footer'
+
+
 
 function App() {
   return (
     <>
       <Navbar />
+      <Carrousel />
       <Productos />
       <Footer />
     </>
