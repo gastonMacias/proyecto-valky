0ed47c50ed97a74ee6667080243a92eb8eced9b3
gastonMacias <gastonezequielmacias@gmail.com>
2025-03-31 18:58:06 -0300

    estilos responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,16 +5,21 @@ import SearchInput from '../searchInput/SearchInput';
 const Navbar = () => {
     return (
         <>
-            <nav className="bg-pink-300 h-40 flex items-center justify-around ">
-                <div className="flex flex-col items-center ">
-                    <img className="size-20 w-fit rounded-4xl  " src="../public/logo/logo sin fondo.jpg" alt="logo valky" />
-                    <h1 className="text-black font-thin text-3xl">Papelería Creativa Valky</h1>
+            <nav className="flex flex-col items-center justify-around min-h-[4rem] bg-pink-300 sm:flex-col lg:flex-row p-4">
+                <div className="flex flex-col items-center text-center">
+                    <img
+                        className="w-16 rounded-xl sm:w-24 sm:rounded-2xl lg:w-32 lg:rounded-4xl"
+                        src="../public/logo/logo sin fondo.jpg"
+                        alt="logo valky"
+                    />
+                    <h1 className="text-lg sm:text-xl lg:text-2xl font-semibold">
+                        Papelería Creativa Valky
+                    </h1>
                 </div>
                 <div>
                     <SearchInput />
                 </div>
             </nav>
-
         </>
     )
 }
