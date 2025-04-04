6e6d4b925f0a67c700669c7914bd52f4944c6e99
gastonMacias <gastonezequielmacias@gmail.com>
2025-04-04 19:18:26 -0300

    orden de los import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react'
 
 import './App.css'
+
 import Navbar from './components/navbar/Navbar'
 import Carrousel from './components/carrousel/Carrousel'
 import Productos from './components/productos/Productos'
 import Footer from './components/footer/Footer'
-import { useState } from 'react'
+
 
 
 
