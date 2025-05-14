e45582c4d5d8282a3f9c7c6a6eaa993dbd9bf5c9
gastonMacias <gastonezequielmacias@gmail.com>
2025-05-14 15:02:00 -0300

    cambio en el grid para responsive

--- src/components/grupoProductos/GrupoProductos.jsx
+++ src/components/grupoProductos/GrupoProductos.jsx
@@ -9,7 +9,7 @@ const GrupoProductos = ({ titulo, productos }) => {
             <h3 className="text-left text-white font-bold text-2xl bg-orange-300 m-3 p-3 rounded">
                 {titulo}
             </h3>
-            <div className="grid grid-cols-2 gap-4 mb-16 md:grid-cols-3 lg:grid-cols-5 lg:mb-20">
+            <div className="grid grid-cols-2 gap-6 mb-16 md:grid-cols-3 lg:grid-cols-4 lg:mb-20">
                 {productos.map((producto, index) => (
                     <Cards key={index} producto={producto} />
                 ))}
